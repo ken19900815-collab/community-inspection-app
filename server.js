ba8863ec771eac7442e9cfeab8ea57c9aac9f064
ken19900815-collab <ken19900815@gmail.com>
2026-04-28 18:05:01 +0800

    Initial upload project

---
+++ server.js
@@ -0,0 +1,114 @@
+[
+  {
+    "id": "L01",
+    "name": "B1庫房",
+    "sort": 1,
+    "enabled": true,
+    "note": ""
+  },
+  {
+    "id": "L02",
+    "name": "B2庫房",
+    "sort": 2,
+    "enabled": true,
+    "note": ""
+  },
+  {
+    "id": "L03",
+    "name": "B3庫房",
+    "sort": 3,
+    "enabled": true,
+    "note": ""
+  },
+  {
+    "id": "L04",
+    "name": "B4庫房",
+    "sort": 4,
+    "enabled": true,
+    "note": ""
+  },
+  {
+    "id": "L05",
+    "name": "A棟一樓安全梯",
+    "sort": 5,
+    "enabled": true,
+    "note": ""
+  },
+  {
+    "id": "L06",
+    "name": "泳池",
+    "sort": 6,
+    "enabled": true,
+    "note": ""
+  },
+  {
+    "id": "L07",
+    "name": "2樓家教室",
+    "sort": 7,
+    "enabled": true,
+    "note": ""
+  },
+  {
+    "id": "L08",
+    "name": "2樓桌球室",
+    "sort": 8,
+    "enabled": true,
+    "note": ""
+  },
+  {
+    "id": "L09",
+    "name": "2樓健身房",
+    "sort": 9,
+    "enabled": true,
+    "note": ""
+  },
+  {
+    "id": "L10",
+    "name": "2樓休息室",
+    "sort": 10,
+    "enabled": true,
+    "note": ""
+  },
+  {
+    "id": "L11",
+    "name": "14樓中繼水塔",
+    "sort": 11,
+    "enabled": true,
+    "note": ""
+  },
+  {
+    "id": "L12",
+    "name": "26樓廚房",
+    "sort": 12,
+    "enabled": true,
+    "note": ""
+  },
+  {
+    "id": "L13",
+    "name": "頂樓水池",
+    "sort": 13,
+    "enabled": true,
+    "note": ""
+  },
+  {
+    "id": "L14",
+    "name": "B棟一樓安全梯",
+    "sort": 14,
+    "enabled": true,
+    "note": ""
+  },
+  {
+    "id": "L15",
+    "name": "B棟頂樓",
+    "sort": 15,
+    "enabled": true,
+    "note": ""
+  },
+  {
+    "id": "L16",
+    "name": "後哨",
+    "sort": 16,
+    "enabled": true,
+    "note": ""
+  }
+]
